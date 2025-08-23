260481d86c0f9051e0cc2064f5fc5e7fd65f8c38
Reshoo-18 <reshooranjan7@gmail.com>
2025-08-23 11:55:01 +0530

    1st commit

---
+++ Basic Questions/AreaOfSq.cpp
@@ -0,0 +1,14 @@
+// find the area of square
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+
+int a;
+
+cin>>a;
+
+int areaOfSq = a*a;
+cout<<"area of square of your input is :"<<areaOfSq<<endl;
+
+};
---
+++ Basic Questions/MinOf2Numbers.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a;
+int b;
+cout<<"enter the value a"<<endl;
+cin>>a;
+
+cout<<"ebter the value of b"<<endl;
+cin>>b;
+
+if(a>b){
+    cout<<"the minimum value is this :"<<b<<endl;
+}else{
+    cout<<"the minimum value is this :"<<a<<endl;
+}
+return 0;
+}
