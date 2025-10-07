43ae84f99f7a77d7789c30325e3af1dfc89d04aa
Reshoo-18 <reshooranjan7@gmail.com>
2025-10-07 12:18:55 +0530

    added

---
+++ ALL TOPICS/VECTORS/3sum_optimal.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+vector<vector<int>> threeSum(vector<int> nums,int n){
+
+     vector<vector<int>> ans;
+     sort(nums.begin(),nums.end());
+     for(int i=0;i<n;i++){
+        int j=i+1; int k=n-1;
+        if(i>0 && nums[i]==nums[i-1]) continue;
+
+        while(j<k){
+            int sum=nums[i]+nums[j]+nums[k];
+            if(sum<0){
+               j++;
+            }else if(sum>0){
+                k--;
+            }else{
+                ans.push_back({nums[i],nums[j],nums[k]});
+                j++;
+                k--;
+                while(j < k&&nums[j]==nums[j-1]) j++;
+                
+                
+            }
+        }
+     }
+return ans;
+}
+
+int main(){
+vector<int> nums = {-1, 0, 1, 2, -1, -4};
+    vector<vector<int>> result = threeSum(nums, nums.size());
+
+    for (auto trip : result) {
+        for (auto x : trip)
+            cout << x << " ";
+        cout << endl;
+    }
+return 0;
+}
