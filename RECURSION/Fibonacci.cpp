df33bd4b7e281dfb8c7c35f91c1b39042491ec2d
Reshoo-18 <reshooranjan7@gmail.com>
2026-01-07 20:06:52 +0530

    recursion

---
+++ RECURSION/Fibonacci.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int Fibonacci(int n){
+    if(n==0||n==1){
+        return n;
+    }
+    return Fibonacci(n-1)+Fibonacci(n-2);
+}
+int main(){
+cout<<Fibonacci(7);
+return 0;
+}
