bda2402d6a316d83bc8affc12849a3f39294d1ee
Reshoo-18 <reshooranjan7@gmail.com>
2026-01-17 12:31:18 +0530

    merge sort

---
+++ ALL TOPICS/SORTINGS/mergeSSort.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void merge(vector<int>& arr, int st, int mid, int end){
+     vector<int> temp;
+      int i=st, j=mid+1;
+      
+      while(i<=mid && j<=end){
+        if(arr[i]<=arr[j]){
+            temp.push_back(arr[i]);
+            i++;
+        }else{
+            temp.push_back(arr[j]);
+            j++;
+        }
+      }
+
+      while(i<=mid){
+         temp.push_back(arr[i]);
+            i++;
+      }
+      while(j<=end){
+        temp.push_back(arr[j]);
+            j++;
+      }
+
+      for(int idx=0;idx<temp.size();idx++){
+        arr[idx+st]=temp[idx];
+      }
+}
+
+
+void mergeSort(vector<int>&arr,int st, int end){
+        if(st<end){
+            int mid=st+(end-st)/2;
+        
+            mergeSort(arr,st,mid); //left
+            mergeSort(arr,mid+1,end); //right
+
+            merge(arr,st,mid,end);
+        }
+}
+
+ 
+int main(){
+    vector<int> arr={81,61,71,56,24,4,3};
+    mergeSort(arr,0,arr.size()-1);
+    for(int val:arr){
+      cout<<val<< " ";
+    }
+    cout<<endl;
+    
+
+return 0;
+}
