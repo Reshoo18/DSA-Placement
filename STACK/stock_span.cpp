226a0592856e3bb810ba6a4abf5e87170d86788e
Reshoo-18 <reshooranjan7@gmail.com>
2026-01-24 12:40:50 +0530

    stock span

--- STACK/stock_span.cpp
+++ STACK/stock_span.cpp
@@ -1,31 +1,75 @@
+// #include<iostream>
+// #include<vector>
+// #include<stack>
+// using namespace std;
+
+
+
+
+// int main(){
+// vector<int> price={100,80,60,70,60,75,85};
+// vector<int> ans(price.size(),0);
+// stack<int> s;
+// int n=price.size();
+// for(int i=0;i<n;i++){
+//    while(s.size()>0 && price[s.top()]<=price[i]){
+//     s.pop();
+//    }
+//    if(s.empty()){
+//     ans[i]=i+1;
+//    }else{
+//     ans[i]=i-s.top();
+//    }
+//    s.push(i);
+// }
+
+// for(int val:ans){
+//     cout<<val<<" ";
+// }
+// cout<<endl;
+// return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
 #include<iostream>
-#include<vector>
 #include<stack>
+#include<vector>
 using namespace std;
 
-
-
-
+ 
 int main(){
-vector<int> price={100,80,60,70,60,75,85};
-vector<int> ans(price.size(),0);
-stack<int> s;
-int n=price.size();
-for(int i=0;i<n;i++){
-   while(s.size()>0 && price[s.top()]<=price[i]){
-    s.pop();
-   }
-   if(s.empty()){
-    ans[i]=i+1;
-   }else{
-    ans[i]=i-s.top();
-   }
-   s.push(i);
-}
+     
+    vector<int> price={100,80,60,70,60,75,85};
+    vector<int> ans(price.size(),0);
+    stack<int> s;
 
-for(int val:ans){
-    cout<<val<<" ";
-}
-cout<<endl;
+    for(int i=0;i<price.size();i++){
+        while(s.size()>0 && price[s.top()]<=price[i]){
+            s.pop();
+        }
+            if(s.empty()){
+               ans[i] =i+1;
+            }else{
+               ans[i]= i-s.top();
+            }
+            s.push(i);
+        
+        
+    }
+    for(int val :ans){
+            cout<< val<<" ";
+        }
+        cout<<endl;
 return 0;
 }
